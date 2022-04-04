00e9b8ebd0c1359bf5008833f444852c02794423
Irina <89231707647@mail.ru>
2022-04-04 21:32:48 +0700

    commit

---
+++ Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 1: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strInput = Console.ReadLine();
+    int answer = int.Parse(strInput);
+    return answer;
+}
+
+int number = Prompt("Сколько чисел необходимо проверить? Введите количество =>");
+Console.WriteLine();
+
+int count = 0;
+
+for (int i = 0; i< number; i++)
+{
+    Console.Write($"Введите число {i+1}: ");
+    int number2 = int.Parse(Console.ReadLine());
+    if(number2>0)
+    count++;
+}
+Console.Write($"Больше нуля => {count}");
+
---
+++ Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 2. Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+
+int b1 = Prompt("Введите параметр b1 = > ");
+
+int b2 = Prompt("Введите параметр b2 = > ");
+
+int k1 = Prompt("Введите параметр k1 = > ");
+
+int k2 = Prompt("Введите параметр k2 = > ");
+
+int x = (b2 - b1) / (k1 - k2);
+
+int y = k1 * x + b1;
+
+Console.WriteLine($"Координата точки пересечения => ({x}, {y})");
